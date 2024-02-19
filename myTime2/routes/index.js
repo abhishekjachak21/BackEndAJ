482523aa6329b1e67d68a15a57b7b2b28974709c
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-02-19 12:39:03 +0530

    18feb_bkend vdo1 sheryains

--- myTime2/routes/index.js
+++ myTime2/routes/index.js
@@ -7,30 +7,48 @@ router.get('/', function(req, res) {
   res.send("Hellow");
 });
 
+
 router.get('/profile', function(req, res) {
-  res.render('index', { namee : 'abhishek' });
+  req.session.ses1 = 'iamses1';
+  req.session.fucked = true ;
+  res.render('index', { namee : "abhishek" });
 });
 
+
+router.get('/chkban',function(req,res){
+ if(req.session.fucked === true) ;
+  res.send("you are fucked up")
+})
+
+/*
 router.get('/err', function(req, res) {
   res.render('error', { namee : 'abhishek' });
 }); 
 
-// router.get("/create",async function(req,res){
+router.get("/create",async function(req,res){
 
-//   const crtdUsr = await userModel.create({           //await -> ah wait mere liye
-//     username: "Abhishek",
-//     Name:"Lalu",
-//     age:23
-//   });
-//   // res.send("Yeh, ban gaya");
-//   res.send(crtdUsr);
-// });
+  const crtdUsr = await userModel.create({           //await -> ah wait mere liye
+    username: "Abhishek",
+    Name:"jachak",
+    age:26
+  });
+  // res.send("Yeh, ban gaya");
+  res.send(crtdUsr);
+});
 
-// router.get("/allUsers", async function(req,res){
-//   const allUsers = await userModel.find();
-//   res.send(allUsers);
-// })
+router.get("/read", async function(req,res){
+  // const remUsers = await userModel.findOne({username:'gagan'});
+  // res.send(remUsers);
+  const allUsers = await userModel.find();
+  res.send(allUsers);
+})
 
+
+router.get("/delete", async function(req,res){
+  let deltdUsr = await userModel.findOneAndDelete({username:'Abhishek'});
+  res.send(deltdUsr);
+});
+*/
 
 module.exports = router;
 
