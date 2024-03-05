5c0796e69a4804488c61b67f744f6a4b033b3940
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-03-05 22:58:48 +0530

    yu

--- bkendbyHitesh/video_4/models/eCommerce/product.models.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose'
-
-const prodSchema = new mongoose.Schema({},{});
-
-export const Product = mongoose.model("Product", prodSchema);
-
--- bkendbyHitesh/video_4/models/todos/sub_todo.models.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-
-const subtodoSchema = new mongoose.Schema({},{timestamps:true})
-
-export const subtodo = mongoose.model("subtodo",subtodoSchema)
