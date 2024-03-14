4d23fd3c5abbf5bd0202f69e3bd73d0c7128b1eb
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-03-15 00:09:56 +0530

    big error solved, URI-URL

--- BEbyHitesh/video_567/src/app.js
+++ BEbyHitesh/video_567/src/app.js
@@ -9,10 +9,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
 
 //routes import
 import {userRouter} from "./routes/user.routes.js"
--- BEbyHitesh/video_567/src/index.js
+++ BEbyHitesh/video_567/src/index.js
@@ -2,9 +2,7 @@ import {app} from "./app.js"
 import connectDB from './DB/index.js';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './env' });
 
-console.log("MongoDB URL:", process.env.MONGODB_URL); // Add this line to log the MongoDB URL
 
 connectDB()
 .then(() => {
