4b35084b07e34bac10d1de1b5e17e590f0ffcdd3
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-05-25 11:02:15 +0530

    clgproj 25 may

--- clgMernProjj/day2/weather.js
+++ clgMernProjj/day2/weather.js
@@ -11,18 +11,11 @@ let api = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${key}&
 request({
     url:api,
     json:true
-},function(error, response, body){
+},function(error,response,body){
     if(error){
-        callback("something went wrong");
-    }
-    else{
-        callback(`Its temp = ${body.main.temp} in ${body.name}`);
-        callback(`Its temp max = ${body.main.temp_max} in ${body.name}`);
-        callback(`Its pressure = ${body.main.pressure} in ${body.name}`);
-        callback(`Its humidity = ${body.main.humidity} in ${body.name}`);
-        callback(`Its sea level = ${body.main.sea_level} in ${body.name}`);
-        callback(`Its ground level = ${body.main.grnd_level} in ${body.name}`);
+       callback('something went wrong');
+    }else{
+        callback(`Its ${body.main.temp}  ${body.name}`);
     }
 })
-
 }
--- clgMernProjj/day2/weatherMain.js
+++ clgMernProjj/day2/weatherMain.js
@@ -1,18 +1,30 @@
- 
+var weather = require("./weather.js");
+var location = require('./location.js')
+var argv = require("yargs")
+  .option("loc", {
+    alias: 'l',
+    demand: false,
+    type: "string",
+    description: 'Enter city to get the weather'
+  })
+  .help("help").argv;
 
-var weather = require('./weather.js')
-
-var argv  = require('yargs')
-.option('loc',{
-    alias:'1',
-    demand:true,
-    type:'string',
-    description:'enter city is must'
-})
-.help('help')
-.argv
-
-
- weather(argv.loc, function(currentWeather){
+if (typeof argv.l === 'string' && argv.l.length > 0) {
+  weather(argv.l, function (currentWeather) {
     console.log(currentWeather);
- })
+  });
+}
+else {
+  console.log('Location not provided, detecting the current city ......!');
+  location(function (location) {
+    if (location) {
+      weather(location.city, function (data) {
+        console.log(data);
+
+      })
+    }
+    else{
+      console.log('unable to guess');
+    }
+  })
+}
